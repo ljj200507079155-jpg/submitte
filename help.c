075718284742f7d3ed8b9f3238926a04619a7d03
Lejiaie <ljj200507079155@outlook.com>
2025-09-27 10:28:48 +0800

    第二次提交

---
+++ help.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void help(){
+    printf("Usege: .\judege [command] \n");
+    printf("example: .\judge --vision\n");
+    printf("command:\n");
+   printf("  --vision: show the version \n");
+    printf("  --help: show this help \n");
+}
